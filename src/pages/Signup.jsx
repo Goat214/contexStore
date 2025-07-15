3c215e33103f4d6c818767587457e8e63b40ff19
Abdulatif <abdulatif@gmail.com>
2025-07-15 06:46:13 +0600

    add

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from "react";
 import { GlobalContext } from "../context/GlobalContext";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Signup() {
   const { userData, dispatch } = useContext(GlobalContext);
